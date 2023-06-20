1a97e43585295d03cccd3855781d1d89923deaa0
shahboz <shahbozmamatkarimov2303@gmail.com>
2023-06-20 14:02:02 +0300

    backend

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 
 const start = async () => {
   try {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, { cors: true });
     const PORT = process.env.PORT || 4000;
     app.use(cookieParser());
     app.setGlobalPrefix('api');
